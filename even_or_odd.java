f28bd297a3e182bafe3d3394bd85fb73ef905dfe
navneetnic28 <149612897+navneetnic28@users.noreply.github.com>
2026-04-08 23:01:18 +0530

    even or odd

---
+++ even_or_odd.java
@@ -0,0 +1,10 @@
+public class even_or_odd {
+    public static void main(String args[]) {
+        int number = 11;
+        if (number % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
