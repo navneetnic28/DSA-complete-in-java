fd6efaec3e705b899e5547773d08d3087ee9c6e8
navneetnic28 <149612897+navneetnic28@users.noreply.github.com>
2026-04-08 23:06:44 +0530

    multiplication

--- even_or_odd.java
+++ even_or_odd.java
@@ -1,6 +1,6 @@
 public class even_or_odd {
     public static void main(String args[]) {
-        int number = 11;
+        int number = 18;
         if (number % 2 == 0) {
             System.out.println("even");
         } else {
